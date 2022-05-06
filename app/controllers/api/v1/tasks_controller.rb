bb23e0dce3dfd7fcfdaec72e70622943a749ebf6
nyount14 <90732548+nyount14@users.noreply.github.com>
2022-05-05 20:38:45 -0500

    Create tasks_controller.rb

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Api
+    module V1
+      # Handles endpoints related to tasks
+      class TasksController < Api::V1::ApplicationController
+        # namespace :mytasks
+        
+        def all  
+        #     get :all
+        end
+
+        def create
+        #     post :create
+        end
+
+        def update
+        #     put :update
+        end
+
+        def delete
+        #     delete :delete
+        end
+
+      end
