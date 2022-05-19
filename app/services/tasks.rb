627b4d5a1aabff77b8b9777c23724d261af59a58
nyount14 <90732548+nyount14@users.noreply.github.com>
2022-05-18 21:38:48 -0500

    CRUD with tasks

--- app/services/tasks.rb
+++ app/services/tasks.rb
@@ -1,12 +1,22 @@
 module Tasks
     def self.new_task(params, current_user)
-        byebug
         # STEP 1 - create task
-
+        task = current_user.tasks.new(params)
         # STEP 2 - save task
-
+        # task.save!
         # STEP 3 - return errors if task not valid, otherwise step 4
-
+        return ServiceContract.error("Error saving task") unless task.save
         # STEP 4 - return success
+        ServiceContract.success(task)
+    end
+
+    def self.update_task(task_id, params)
+        # STEP 1 - get the task we need to update with id
+        task = Task.find(task_id)
+        # STEP 2 - update
+        # STEP 3 - return error if update was unsuccessful
+        return ServiceContract.error("Task did not save successfully") unless task.update(params)
+        # STEP 4 - return success if update was successful
+        ServiceContract.success(task)
     end
 end
